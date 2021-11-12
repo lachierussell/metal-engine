aa0cfdc993362c15116cc1cecbc58b3df17fdf7f
Lachlan Russell <lachie.russell@gmail.com>
2021-11-12 22:28:46 +1100

    Initial Commit

---
+++ MetalEngine/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  MetalEngine
+//
+//  Created by Lachlan Russell on 12/11/21.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ MetalEngine/GameViewController.h
@@ -0,0 +1,16 @@
+//
+//  GameViewController.h
+//  MetalEngine
+//
+//  Created by Lachlan Russell on 12/11/21.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <Metal/Metal.h>
+#import <MetalKit/MetalKit.h>
+#import "Renderer.h"
+
+// Our macOS view controller.
+@interface GameViewController : NSViewController
+
+@end
